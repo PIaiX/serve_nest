7c7e3abd9c1772650c8f4c2bcc07dec20ed8a83f
Rus <61886636+rus-sharafiev@users.noreply.github.com>
2023-05-15 13:45:45 +0300

    Added: new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common'
 import { UsersModule } from './users/users.module'
 import { AuthModule } from './auth/auth.module'
-import { CartModule } from './cart/cart.module'
-import { ProductsModule } from './products/products.module'
+import { OrdersModule } from './orders/orders.module'
+import { CategoriesModule } from './categories/categories.module'
+import { SubcategoriesModule } from './subcategories/subcategories.module'
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
-    CartModule,
-    ProductsModule
+    OrdersModule,
+    CategoriesModule,
+    SubcategoriesModule
   ]
 })
 export class AppModule { }
