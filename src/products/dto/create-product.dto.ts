c25cba087a614fb0db35ab7765807419ae3fa63c
Rus <61886636+rus-sharafiev@users.noreply.github.com>
2023-05-15 13:43:06 +0300

    Removed: Order and cart

--- src/products/dto/create-product.dto.ts
+++
@@ -1,4 +0,0 @@
-import { OmitType } from "@nestjs/swagger"
-import { Product } from "../entities/product.entity"
-
-export class CreateProductDto extends OmitType(Product, ['id', 'createdBy', 'updatedAt', 'updatedBy'] as const) { }
