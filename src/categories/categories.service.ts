3db117ac3c1aedc74c7cf908ceb84135cceffb70
Rus <61886636+rus-sharafiev@users.noreply.github.com>
2023-05-22 15:53:14 +0300

    Add Specialties count to find all

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -5,39 +5,52 @@ import { PrismaService } from 'src/_common/prisma/prisma.service'
 
 @Injectable()
 export class CategoriesService {
-  constructor(private readonly prismaService: PrismaService) { }
+    constructor(private readonly prismaService: PrismaService) { }
 
-  create(createCategoryDto: CreateCategoryDto) {
-    return this.prismaService.category.create({
-      data: createCategoryDto
-    })
-  }
+    create(createCategoryDto: CreateCategoryDto) {
+        return this.prismaService.category.create({
+            data: createCategoryDto
+        })
+    }
 
-  findAll() {
-    return this.prismaService.category.findMany({
-      include: {
-        subcategories: true
-      }
-    })
-  }
+    findAll() {
+        return this.prismaService.category.findMany({
+            include: {
+                subcategories: {
+                    include: {
+                        _count: {
+                            select: {
+                                specialties: true
+                            }
+                        }
+                    },
+                    orderBy: {
+                        specialties: {
+                            _count: 'desc'
+                        }
+                    }
+                }
+            }
+        })
+    }
 
-  findOne(id: number) {
-    return this.prismaService.category.findUnique({
-      where: { id }
-    })
-  }
+    findOne(id: number) {
+        return this.prismaService.category.findUnique({
+            where: { id }
+        })
+    }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return this.prismaService.category.update({
-      where: { id },
-      data: updateCategoryDto
-    })
-  }
+    update(id: number, updateCategoryDto: UpdateCategoryDto) {
+        return this.prismaService.category.update({
+            where: { id },
+            data: updateCategoryDto
+        })
+    }
 
-  remove(id: number) {
-    return this.prismaService.category.delete({
-      where: { id }
-    })
-  }
+    remove(id: number) {
+        return this.prismaService.category.delete({
+            where: { id }
+        })
+    }
 
 }
