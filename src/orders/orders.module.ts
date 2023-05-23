5740c7ceeb9dc568a18f124e1b815ac35f33e6a2
Rus <61886636+rus-sharafiev@users.noreply.github.com>
2023-05-23 14:48:43 +0300

    Import jwt module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common'
 import { OrdersService } from './orders.service'
 import { OrdersController } from './orders.controller'
 import { PrismaModule } from 'src/_common/prisma/prisma.module'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
   controllers: [OrdersController],
   providers: [OrdersService],
-  imports: [PrismaModule]
+  imports: [PrismaModule, JwtModule],
+  exports: [OrdersService]
 })
 export class OrdersModule { }
