ee1ed12d6bb9c4dd7f8cf87c106d3b65f3452951
Rus <61886636+rus-sharafiev@users.noreply.github.com>
2023-05-23 14:47:53 +0300

    Add create and remove order responses

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,34 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { OrdersService } from './orders.service';
-import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+import { FastifyRequest } from 'fastify'
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseGuards, Req } from '@nestjs/common'
+import { OrdersService } from './orders.service'
+import { CreateOrderDto } from './dto/create-order.dto'
+import { UpdateOrderDto } from './dto/update-order.dto'
+import { OrderQueryParams } from './entities/order.entity'
+import { AuthGuard } from 'src/_common/guards/auth.guard'
 
 @Controller('orders')
+@UseGuards(AuthGuard)
 export class OrdersController {
-  constructor(private readonly ordersService: OrdersService) {}
+    constructor(private readonly ordersService: OrdersService) { }
 
-  @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersService.create(createOrderDto);
-  }
+    @Post()
+    create(@Body() createOrderDto: CreateOrderDto) {
+        return this.ordersService.create(createOrderDto);
+    }
 
-  @Get()
-  findAll() {
-    return this.ordersService.findAll();
-  }
+    @Get()
+    findAll(@Query() params: OrderQueryParams) {
+        return this.ordersService.findAll(params)
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ordersService.findOne(+id);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.ordersService.findOne(+id);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.ordersService.update(+id, updateOrderDto);
-  }
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
+        return this.ordersService.update(+id, updateOrderDto);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ordersService.remove(+id);
-  }
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.ordersService.remove(+id);
+    }
+
+    @Post(':id/response')
+    respond(@Req() request: FastifyRequest, @Param('id') id: string) {
+        return this.ordersService.respond(+id, +request.headers.userId);
+    }
+
+    @Delete(':id/response')
+    removeResponse(@Req() request: FastifyRequest, @Param('id') id: string) {
+        return this.ordersService.removeResponse(+id, +request.headers.userId);
+    }
+
 }
