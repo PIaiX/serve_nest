64badf5d80737132c132c8eb201d2c8991c58441
Rus <61886636+rus-sharafiev@users.noreply.github.com>
2023-06-05 18:59:24 +0300

    Add profile examples

--- src/profiles/dto/update-profile.dto.ts
+++ src/profiles/dto/update-profile.dto.ts
@@ -1,6 +1,17 @@
 import { PartialType, PickType } from '@nestjs/swagger'
 import { Profile } from '../entities/profile.entity'
+import Prisma from '@prisma/client'
 
 export class UpdateProfileDto extends PartialType(PickType(Profile, ['userId'] as const)) {
     isVisible?: boolean
+    examples?: Omit<Example, 'id' | 'profileUserId'>[]
+}
+
+class Example implements Prisma.Example {
+    id: number
+    profileUserId: number
+    image: string | null
+    title: string | null
+    description: string | null
+    price: number | null
 }
