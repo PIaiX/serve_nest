0d4fed0488c87a102c36edf6588224441029c0fb
Rus <61886636+rus-sharafiev@users.noreply.github.com>
2023-06-19 19:01:05 +0300

    Add create message and find user chats

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -9,9 +9,23 @@ import { PrismaService } from 'src/_common/prisma/prisma.service'
 export class MessagesService {
     constructor(private readonly prismaService: PrismaService) { }
 
-    create(createMessageDto: CreateMessageDto) {
+    async create(createMessageDto: CreateMessageDto) {
+        const { senderId, recipientId, ...rest } = createMessageDto
+        const userOneId = senderId < recipientId ? senderId : recipientId
+        const userTwoId = senderId > recipientId ? senderId : recipientId
         return this.prismaService.chatMessages.create({
-            data: createMessageDto
+            data: {
+                ...rest,
+                sender: { connect: { id: senderId } },
+                recipient: { connect: { id: recipientId } },
+                chat: {
+                    connectOrCreate: {
+                        where: { userOneId_userTwoId: { userOneId, userTwoId } },
+                        create: { userOneId, userTwoId }
+                    }
+                }
+            }
+
         })
     }
 
@@ -31,5 +45,32 @@ export class MessagesService {
 
     remove(id: number) {
         return `This action removes a #${id} message`
+    }
+
+    findAllChats(userId: number) {
+        const getName = {
+            select: {
+                id: true,
+                firstName: true,
+                avatar: true
+            }
+        }
+        return this.prismaService.chat.findMany({
+            where: {
+                OR: [
+                    { userOneId: userId },
+                    { userTwoId: userId },
+                ]
+            },
+            include: {
+                userOne: getName,
+                userTwo: getName,
+                messages: {
+                    take: 1,
+                    orderBy: { createdAt: 'desc' },
+                    include: { sender: getName }
+                }
+            }
+        })
     }
 }
